0953d99fa5b9944ad2b7981d6de6b99829dff10f
Designer-space <123vinaymaurya@gmail.com>
2024-06-28 13:23:59 +0530

    add the assets and complete the ui part

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import HomePage from "./pages/home/HomePage.jsx";
+import LoginPage from "./pages/auth/login/LoginPage.jsx";
+import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{ path: "/", element: <HomePage /> },
+			{ path: "login", element: <LoginPage /> },
+			{ path: "signup", element: <SignUpPage /> },
+			{ path: "notifications", element: <NotificationPage /> },
+			{ path: "profile/:username", element: <ProfilePage /> },
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router}>
+			<App />
+		</RouterProvider>
+	</React.StrictMode>
+);
