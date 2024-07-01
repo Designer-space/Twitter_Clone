19e47860618218642778b3fe190e2a1abcd34faf
Designer-space <123vinaymaurya@gmail.com>
2024-07-01 18:40:09 +0530

    add delete post route

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -5,19 +5,48 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
+
+	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+	const queryClient = useQueryClient();
+
 	const postOwner = post.user;
 	const isLiked = false;
 
-	const isMyPost = true;
+	const isMyPost = authUser._id === post.user._id;
 
 	const formattedDate = "1h";
 
 	const isCommenting = false;
 
-	const handleDeletePost = () => {};
+	const { mutate: deletePost, isPending } = useMutation({
+		mutationFn: async (id) => {
+			try {
+				const res = await fetch(`/api/posts/${id}`, {
+					method: "DELETE",
+				});
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Post deleted successfully");
+			queryClient.invalidateQueries({ queryKey: ["post"] });
+		},
+	});
+
+	const handleDeletePost = () => {
+		deletePost(post._id);
+	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
@@ -53,10 +82,14 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								<FaTrash
-									className='cursor-pointer hover:text-red-500'
-									onClick={handleDeletePost}
-								/>
+								{isPending ? (
+									<LoadingSpinner size='sm' />
+								) : (
+									<FaTrash
+										className='cursor-pointer hover:text-red-500'
+										onClick={handleDeletePost}
+									/>
+								)}
 							</span>
 						)}
 					</div>
--- frontend/src/hooks/useAuthUser.js
+++ frontend/src/hooks/useAuthUser.js
@@ -12,7 +12,6 @@ const useAuthUser = () => {
         if (!res.ok) {
           throw new Error(data.error || "Something went wrong");
         }
-        console.log("authUser is here:", data);
         return data;
       } catch (error) {
         throw new Error(error);
