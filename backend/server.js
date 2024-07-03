9ad21e52bce4c5a9a97553c0cacee85c3b39e1ed
Designer-space <123vinaymaurya@gmail.com>
2024-07-03 16:23:39 +0530

    fix the typo

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 })
