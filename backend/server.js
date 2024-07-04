6a7f59cfd677ba955b5ef71ecd7aa1dfac1c3cb0
Designer-space <123vinaymaurya@gmail.com>
2024-07-04 11:17:29 +0530

    update code for deployment in test branch

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 
 import authRoutes from "./routes/authRoute.js"
 import userRoutes from "./routes/userRoute.js"
@@ -18,8 +19,9 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ extended: true }))
@@ -29,6 +31,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoute)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
